ceee96cc53f21d19d6266cd210434c9a7a37966d
Kacper <74687092+duszeek@users.noreply.github.com>
2022-12-04 18:10:09 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,4 +5,4 @@ Config.Info = {
     ["zielony"]     = {code = "zielony",    grade = 1, hookColor = "65310"}, -- 1
     ["czerwony"]    = {code = "czerwony",   grade = 1, hookColor = "16711680"}, -- 6
     ["czarny"]      = {code = "czarny",   grade = 1, hookColor = "0"}, -- 10
-} 
+}
